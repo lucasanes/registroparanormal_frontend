b5ec00fd89506d3d672f14e747a9b5c9bb5dc9cd
SkyLuke11 <sky.lucas003@gmail.com>
2023-11-04 11:04:03 -0300

    teste2

--- src/pages/Sessao/styles.js
+++ src/pages/Sessao/styles.js
@@ -1,22 +1,22 @@
 import { keyframes, styled } from "../../stitches.config";
 
 export const Container = styled("div", {
-    width: "100%",
-    height: "100vh",
-    background: "rgb(20,20,20)",
-    display: 'flex',
-    flexDirection: 'column'
+  width: "100%",
+  height: "calc(100% - 72px)",
+  background: "rgb(20,20,20)",
+  display: 'flex',
+  flexDirection: 'column'
 });
 
 export const Body = styled("div", {
-    overflowY: "auto",
-    width: '100%',
-    height: 'calc(100% - 72px)',
-    padding: "3rem 2rem 1rem 2rem",
+  overflowY: "auto",
+  width: '100%',
+  height: '100%',
+  padding: "3rem 2rem 1rem 2rem",
 
-    hr: {
-        borderColor: "#ffffff75",
-    },
+  hr: {
+      borderColor: "#ffffff75",
+  },
 });
 
 export const DoubleParteContainer = styled("div", {

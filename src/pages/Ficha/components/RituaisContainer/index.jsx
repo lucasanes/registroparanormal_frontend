14eb156bba976ccbde799c6193ce01cd6c8e2371
SkyLuke11 <sky.lucas003@gmail.com>
2023-11-13 22:06:52 -0300

    ac

--- src/pages/Ficha/components/RituaisContainer/index.jsx
+++ src/pages/Ficha/components/RituaisContainer/index.jsx
@@ -50,9 +50,9 @@ export function RituaisContainer({ data }) {
 
   function comparar(a, b) {
     // Primeiro, compare pelo item "círculo"
-    if (a.círculo < b.círculo) {
+    if (a.circulo < b.circulo) {
       return -1;
-    } else if (a.círculo > b.círculo) {
+    } else if (a.circulo > b.circulo) {
       return 1;
     } else {
       // Se os círculos forem iguais, compare pelo elemento

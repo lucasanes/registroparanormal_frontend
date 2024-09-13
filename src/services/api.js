7472328ef1524aa3a4a3d2a1312fb2eb0303eee8
Lucas (Lightz) <sky.lucas003@gmail.com>
2024-09-13 14:44:08 -0300

    feat: add .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://projetorpgbackend-production.up.railway.app/"
-  baseURL: "http://localhost:8080"
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080"
 });

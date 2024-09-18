fe7320bf501688adf06df26ee1d7c36f0eb38462
Lucas (Lightz) <sky.lucas003@gmail.com>
2024-09-18 14:47:50 -0300

    feat: add permission and no devices

--- src/pages/Webcam/components/ModalWebcam/index.jsx
+++ src/pages/Webcam/components/ModalWebcam/index.jsx
@@ -4,61 +4,71 @@ import { Body, Button, Container, Header } from "./styles";
 export function ModalWebcam({ setModalClose }) {
 
   const [webcams, setWebcams] = useState([]);
+  const [permissionDenied, setPermissionDenied] = useState(false);
   const deviceId = localStorage.getItem("@registroparanormal:camera");
 
-  useEffect(() => {  
-    navigator.mediaDevices.enumerateDevices().then(devices => {
-      const webcams = devices.filter(device => device.kind === 'videoinput')
-      setWebcams(webcams)
-    })
-  }, [])
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(() => {
+        navigator.mediaDevices.enumerateDevices().then(devices => {
+          const webcams = devices.filter(device => device.kind === 'videoinput');
+          setWebcams(webcams);
+        });
+      })
+      .catch(() => {
+        setPermissionDenied(true);
+      });
+  }, []);
 
   async function handleCreate(deviceId) {
-
     localStorage.setItem("@registroparanormal:camera", deviceId);
-
-    setModalClose()
-
+    setModalClose();
   }
 
   return (
     <Container>
 
       <Header>
-
         <h1>Selecionar Webcam</h1>
         <button type="button" onClick={setModalClose}>x</button>
-
       </Header>
 
       <hr />
 
       <Body>
-
         {
-          webcams.map(webcam => (
-            deviceId == webcam.deviceId && <Button 
-              key={webcam.deviceId} 
-              active={true} 
-              onClick={() => handleCreate(webcam.deviceId)}
-              disabled={true}>
-                {webcam.label}
-            </Button>
-          ))
+          permissionDenied ? (
+            <p>Permissão de acesso à câmera negada. Por favor, habilite para continuar.</p>
+          ) : (
+            webcams.length > 0 ? (
+              <>
+                {
+                  webcams.map(webcam => (
+                    deviceId === webcam.deviceId ? (
+                      <Button 
+                        key={webcam.deviceId} 
+                        active={true} 
+                        onClick={() => handleCreate(webcam.deviceId)}
+                        disabled={true}>
+                          {webcam.label}
+                      </Button>
+                    ) : (
+                      <Button 
+                        key={webcam.deviceId} 
+                        active={false} 
+                        onClick={() => handleCreate(webcam.deviceId)}
+                        disabled={false}>
+                          {webcam.label}
+                      </Button>
+                    )
+                  ))
+                }
+              </>
+            ) : (
+              <p>Nenhuma webcam encontrada.</p>
+            )
+          )
         }
-
-        {
-          webcams.map(webcam => (
-            deviceId != webcam.deviceId && <Button 
-              key={webcam.deviceId} 
-              active={false} 
-              onClick={() => handleCreate(webcam.deviceId)}
-              disabled={false}>
-                {webcam.label}
-            </Button>
-          ))
-        }
-
       </Body>
 
     </Container>
